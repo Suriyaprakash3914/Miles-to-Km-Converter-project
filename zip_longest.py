005190dfcff679f5b1cab8e9edfc67750e2b9a4a
Suriyaprakash <v.suriyaprakash3914@gmail.com>
2026-03-13 15:36:29 +0530

    udemy.py

--- zip_longest.py
+++
@@ -1,9 +0,0 @@
-from itertools import zip_longest
-name = ['surya', 'dk', 'saran', 'mahi', 'vikash', 'jayashri']
-salary = [1000, 1234, 2345, 4532]
-d = {}
-for i, j in zip_longest(name, salary, fillvalue=0):
-    key = i
-    value = j+j*5//100
-    d[key] = value
-print(d)
